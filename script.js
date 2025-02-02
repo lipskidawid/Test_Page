704a49a6f613fde4a87a1460619aae74f4db5e8c
Dawid Lipski <lipski0dawid@gmail.com>
2025-02-02 18:42:09 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Google Analytics
+(function() {
+  let script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX";
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XXXXXXXXXX');
+})();
+
 // Prosta obsługa animacji przy przewijaniu
 document.addEventListener('DOMContentLoaded', () => {
   const sections = document.querySelectorAll('section');
